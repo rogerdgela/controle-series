456eeeb55d5d97b84cbfc4b8de39ba4fa1f82d38
Rogerio Silva <roger.alves.silva84@gmail.com>
2025-04-27 08:53:34 -0300

    adiciona arquivos de configuração do Laravel Mix, incluindo o package-lock.json, e atualiza o package.json para incluir dependências do Bootstrap e SASS. Implementa a estrutura de arquivos para compilação de CSS e JS, além de integrar os scripts e estilos na view layout.blade.php.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} - Controle de Séries</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}"></script>
 </head>
 <body>
 <h1>Séries</h1>
