e0121a73efae917b870d100508dc99124df40160
Rogerio Silva <roger.alves.silva84@gmail.com>
2025-04-29 20:00:53 -0300

    refatora o método index do SeriesController para utilizar Eloquent na consulta e atualiza o método store para salvar séries usando o modelo Serie

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,7 +10,7 @@ class SeriesController extends Controller
 {
     public function index()
     {
-        $series = DB::select('select * from series');
+        $series = Serie::query()->orderBy('nome')->get();
 
         return view('series.index')->with('series', $series);
     }
@@ -22,7 +23,11 @@ class SeriesController extends Controller
     public function store(Request $request)
     {
         $nomeSerie = $request->input('nome');
-        DB::insert('insert into series (nome) values (?)', [$nomeSerie]);
+
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
         return redirect('/series');
     }
 }
