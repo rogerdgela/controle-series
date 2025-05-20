f23f600b2a3672dea6d747859beaff9ff5eaa685
Rogerio Silva <dgelask8@gmail.com>
2025-05-20 19:58:46 -0300

    Mais sobre Models

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -10,7 +10,7 @@ class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = Serie::query()->orderBy('nome')->get();
+        $series = Serie::all();
         $mensagemSucesso = session('mensagem.sucesso');
 
         return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -4,14 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Serie extends Model
 {
     use HasFactory;
     protected $fillable = ['nome'];
+    protected $with = ['temporadas'];
 
     public function temporadas()
     {
         return $this->hasMany(Season::class, 'series_id');
+    }
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered', function (Builder $queryBuilder) {
+            $queryBuilder->orderBy('nome');
+        });
     }
 }
