9d4c009c0d660fe3d85b909370810233eea94942
Rogerio Silva <roger.alves.silva84@gmail.com>
2025-05-26 19:47:36 -0300

    Bulk insert

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
 use App\Models\Series;
 use Illuminate\Http\Request;
 
@@ -23,19 +25,31 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = Series::create($request->only(['name']));
+        $serie = Series::create($request->all());
+
+        $seasons = [];
 
         for ($i = 1; $i <= $request->seasonsQtd; $i++) {
-            $season = $serie->seasons()->create([
+            $seasons[] = [
+                'series_id' => $serie->id,
                 'number' => $i,
-            ]);
+            ];
+        }
+        
+        Season::insert($seasons);
 
+        $episodes = [];
+        foreach ($serie->seasons as $season) {
             for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                $season->episodes()->create([
+                $episodes[] = [
+                    'season_id' => $season->id,
                     'number' => $j,
-                ]);
+                ];
             }
         }
+        
+        Episode::insert($episodes);
+
 
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série \"{$serie->name}\" adicionada com sucesso!");
