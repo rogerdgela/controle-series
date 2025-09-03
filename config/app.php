7f6a0d0dd3a7978bb840549c8bec98a30306a833
Rogerio Silva <dgelask8@gmail.com>
2025-09-03 17:46:23 -0300

    Service Container e Providers

--- config/app.php
+++ config/app.php
@@ -195,6 +195,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Repository Providers
+        App\Providers\SeriesRepositoryProvider::class,
     ],
 
     /*
